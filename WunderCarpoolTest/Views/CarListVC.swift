bc85adaea4d3078b4c5e5cf5e4818dc923a8aeb8
Abhishek Chaudhari <jax@Abhisheks-MacBook-Pro.local>
2018-08-29 09:35:27 +0530

    Basic setup test

---
+++ WunderCarpoolTest/Views/CarListVC.swift
@@ -0,0 +1,25 @@
+//
+//  CarListVC.swift
+//  WunderCarpoolTest
+//
+//  Created by Abhishek Chaudhari on 29/08/18.
+//  Copyright © 2018 Abhishek Chaudhari. All rights reserved.
+//
+
+import UIKit
+
+class CarListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
